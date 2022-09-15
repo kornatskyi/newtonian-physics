18db640806b4ac2dbabbde374ed3cdecd823c74e
bohdan_kornatskyi <bohdan.kornatskyi@gmail.com>
2022-09-14 22:27:00 -0700

    converting to multiobject simulation

--- src/classes/GravitationalSystem.cpp
+++ src/classes/GravitationalSystem.cpp
@@ -1,29 +1,29 @@
 #include "../../include/CosmicBody.hpp"
 #include "../../include/GravitationalSystem.hpp"
 
-GravitationalSystem::GravitationalSystem(sf::Vector2f center) : body1(30.f, 100000000000), body2(30.f, 1)
+GravitationalSystem::GravitationalSystem(sf::Vector2f center)
 {
+
     this->center = center;
     counter = 0;
 
-    body1.setFillColor(sf::Color(105, 143, 63));
-    body2.setFillColor(sf::Color(10, 18, 42));
-    setPositionOnA1DimensionalPlot(-50.f, body1);
-    setPositionOnA1DimensionalPlot(50.f, body2);
-    body2.setVelocity(sf::Vector2f(0.2, 0.1));
-
     secondConunt = 0.0f;
     seconds = 0.f;
+
+    bodies = new std::vector<CosmicBody>();
+
+    bodies->push_back(CosmicBody(20.f, 115125.f));
+    bodies->at(0).setCenterPosition(sf::Vector2f(500.f, 500.f));
+    bodies->push_back(CosmicBody(100.f, 11902598189758.f));
+    bodies->at(1).setCenterPosition(sf::Vector2f(700.f, 150.f));
+    bodies->push_back(CosmicBody(50.f, 12978410.f));
+    bodies->at(2).setCenterPosition(sf::Vector2f(300.f, 550.f));
 }
 
-void GravitationalSystem::setPositionOnA1DimensionalPlot(float coordinate, CosmicBody &body)
+GravitationalSystem::~GravitationalSystem()
 {
-    body.setCenterPosition(sf::Vector2f(this->center.x + coordinate, this->center.y));
-}
-
-float GravitationalSystem::getPositionOnA1DimensionalPlot(CosmicBody &body)
-{
-    return body.getCenterPosition().x - this->center.x;
+    std::cout << "Destructor executed" << std::endl;
+    delete bodies;
 }
 
 float GravitationalSystem::distnceBetweenTwoPoints(sf::Vector2f point1, sf::Vector2f point2)
@@ -38,7 +38,6 @@ float GravitationalSystem::calcForce(float m1, float m2, float d)
 
 sf::Vector2f GravitationalSystem::calculateForce(float mass1, float mass2, sf::Vector2f d)
 {
-
     float absDisplacement = std::sqrt(std::pow(d.x, 2) + std::pow(d.y, 2));
     float absForce = ((GRAVITY_CONSTANT * mass1 * mass2) / (d.x * d.x + d.y * d.y));
     sf::Vector2f force(absForce * (d.x / absDisplacement), absForce * (d.y / absDisplacement));
@@ -52,7 +51,7 @@ sf::Vector2f GravitationalSystem::calculateRadiusVector(sf::Vector2f point1, sf:
 
 void GravitationalSystem::update()
 {
-    if (counter > 10) // count 100 update loop iterations intead of using time class
+    if (counter > 100) // count 100 update loop iterations intead of using time class
     {
 
         deltaTime = clock.getElapsedTime();
@@ -60,28 +59,34 @@ void GravitationalSystem::update()
 
         seconds += deltaTime.asSeconds();
         informationDisplayer.setTextField("Seconds", "Seconds:" + std::to_string(seconds));
-        informationDisplayer.setTextField("positionBody1", "body1 position: x: " + InformationDisplayer::numberToString(body1.getPosition().x) + "  y:" + InformationDisplayer::numberToString(body1.getPosition().y));
-        informationDisplayer.setTextField("positionBody2", "body2 position: x: " + InformationDisplayer::numberToString(body2.getPosition().x) + "  y:" + InformationDisplayer::numberToString(body2.getPosition().y));
-        informationDisplayer.setTextField("forceBody1", "body1 experienced force: x: " + InformationDisplayer::numberToString(body1.getForce().x) + "  y:" + InformationDisplayer::numberToString(body1.getForce().y));
-        informationDisplayer.setTextField("forceBody2", "body2 experienced force: x: " + InformationDisplayer::numberToString(body2.getForce().x) + "  y:" + InformationDisplayer::numberToString(body2.getForce().y));
-        informationDisplayer.setTextField("velocityBody1", "body1 velocity: x: " + InformationDisplayer::numberToString(body1.getVelocity().x) + "  y:" + InformationDisplayer::numberToString(body1.getVelocity().y));
-        informationDisplayer.setTextField("velocityBody2", "body2 velocity: x: " + InformationDisplayer::numberToString(body2.getVelocity().x) + "  y:" + InformationDisplayer::numberToString(body2.getVelocity().y));
 
-        // draw bodies' names
-        informationDisplayer.setTextField("body1", "body1", body1.getPosition());
-        informationDisplayer.setTextField("body2", "body2", body2.getPosition());
+        // sf::Vector2f d = calculateRadiusVector(body1.getPosition(), body2.getPosition());
+        // sf::Vector2f force = calculateForce(body1.getMass(), body2.getMass(), d);
+        // body1.applyForce(-force);
+        // body2.applyForce(force);
 
-        sf::Vector2f d = calculateRadiusVector(body1.getPosition(), body2.getPosition());
-        sf::Vector2f force = calculateForce(body1.getMass(), body2.getMass(), d);
-        body1.applyForce(-force);
-        body2.applyForce(force);
+        for (long unsigned int i = 0; i < bodies->size(); i++)
+        {
+            for (long unsigned int j = 0; j < bodies->size(); j++)
+            {
+                if (&bodies->at(i) != &bodies->at(j))
+                {
+                    sf::Vector2f d = calculateRadiusVector(bodies->at(i).getPosition(), bodies->at(j).getPosition());
+                    sf::Vector2f force = calculateForce(bodies->at(i).getMass(), bodies->at(j).getMass(), d);
+                    bodies->at(i).applyForce(-force);
+                    bodies->at(j).applyForce(force);
+                    bodies->at(i).functionOverTime(1);
+                    bodies->at(j).functionOverTime(1);
+                }
+            }
+        }
 
         // call time dependent functions on the bodies to calculate it's characteristics
         // body1.functionOverTime(deltaTime.asMicroseconds());
 
         // body2.functionOverTime(deltaTime.asMicroseconds());
-        body1.functionOverTime(1);
-        body2.functionOverTime(1);
+        // body1.functionOverTime(1);
+        // body2.functionOverTime(1);
 
         counter = 0;
     }
@@ -96,23 +101,24 @@ float GravitationalSystem::deltaDisplacement(float speed, float time)
 }
 void GravitationalSystem::draw(sf::RenderTarget &target, sf::RenderStates states) const
 {
-    target.draw(body1, states);
-    target.draw(body2, states);
     target.draw(informationDisplayer, states);
-    // target.draw(planetSpeedText);
-    // target.draw(planetAccselerationText);
+
+    for (long unsigned int i = 0; i < bodies->size(); i++)
+    {
+        target.draw(bodies->at(i), states);
+    }
 
     // Draw a center
     sf::CircleShape point(2.f);
     point.setPosition(sf::Vector2f(center.x - 1, center.y - 1));
     point.setFillColor(sf::Color::Black);
     target.draw(point);
-    // Draw an axis
-    sf::RectangleShape axis(sf::Vector2f(1500.f, 2));
-    axis.setPosition(sf::Vector2f(center.x - 750.f, center.y - 1));
-    axis.setFillColor(sf::Color::Black);
+    // // Draw an axis
+    // sf::RectangleShape axis(sf::Vector2f(1500.f, 2));
+    // axis.setPosition(sf::Vector2f(center.x - 750.f, center.y - 1));
+    // axis.setFillColor(sf::Color::Black);
 
-    target.draw(axis);
+    // target.draw(axis);
 }
 
 void GravitationalSystem::setCenter(sf::Vector2f center)
